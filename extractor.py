b98d566c20243e2a251fb48abbd551026c5bb339
ShedrachIkenna <Shedrach686@gmail.com>
2026-03-22 23:15:28 +0100

    added ResidualStreamExtractor constructor function

--- extractor.py
+++ extractor.py
@@ -16,3 +16,13 @@ class ResidualStreamExtractor:
     wpe: word position embedding 
     L + 1: L+1 layer states total for L blocks 
     """
+
+    def __init__(self, model_name: str = "gpt2"): 
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name) # tokenizer for GPT-2
+        self.model = AutoModelCausalLM.from_pretrained(model_name) # GPT-2 model weights 
+        self.model.eval() # tells pytorch that the model is used for inference not training 
+
+        self._residual_stack: list[torch.Tensor] = [] # Where we store the residual we get from a layer 
+        self._hooks: list[torch.utils.hooks.RemovableHook] = [] # saves a reference of pytorch internal record of the _hook_fn function which we will use for cleanup 
+
+        self._register_hooks()
